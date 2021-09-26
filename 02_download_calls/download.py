cebb00b43cdf7279b4285785438a47962d019944
Cong Nguyen <rampadc@users.noreply.github.com>
2021-09-27 02:18:43 +1000

    Updated download script to use stage 1's output csv

--- 02_download_calls/download.py
+++ 02_download_calls/download.py
@@ -1,10 +1,15 @@
 import csv
+import os
 from urllib.request import urlretrieve
 
-with open('frogs.csv') as csvfile:
+file_path = os.path.join( os.path.dirname( __file__ ), '..', '01_scrape_frog_calls', 'frogs.csv' )
+save_folder = os.path.join( os.path.dirname( __file__ ), 'calls')
+
+with open(file_path) as csvfile:
   reader = csv.reader(csvfile)
-  next(reader, None);
+  next(reader, None)
   for row in reader:
     if row[2] == 'null':
       continue
-    urlretrieve(row[2], f'{row[0]} - {row[1]}.m4a')
+    file_name = os.path.join(save_folder, f'{row[0]} - {row[1]}.m4a')
+    urlretrieve(row[2], file_name)
